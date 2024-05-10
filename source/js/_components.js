fb828dc6a56fa5c22a9857d39cad18f4bd1cf9bb
roman <romasaab@gmail.com>
2024-05-10 05:55:19 +0300

    add project

---
+++ source/js/_components.js
@@ -0,0 +1,18 @@
+import './components/dinamicHeight';
+import './components/animateBtn';
+import './components/accordion';
+import './components/range';
+import './components/animations';
+import './components/tabs';
+import './components/burger';
+import './components/modals';
+import './components/select';
+import './components/card';
+import './components/showPass';
+import './components/preloader';
+
+
+
+
+
+
